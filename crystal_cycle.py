94807e0cecae6ba545ecc1fa8d851374248a7312
TheCodingGuy0error0 <sebastian.minciunalux@gmail.com>
2025-11-14 13:52:32 +0100

    y

--- crystal_cycle.py
+++ crystal_cycle.py
@@ -1,15 +1,34 @@
 from pynput.keyboard import Controller as KeyboardController, Key
 from pynput.mouse import Button, Controller as MouseController
 import time
-
-mouse = MouseController()
-keyboard = KeyboardController()
+import threading
 
 
-def crystal_cycle()
-    mouse.click(Button.left)
-    time.sleep(0.05)
-    mouse.click(Button.right)
-    time.sleep(0.05)
+running = False
+cycle_thread = None
 
+def crystal_cycle():
+    while running:
+        mouse.click(Button.left)
+        time.sleep(0.05)
+        mouse.click(Button.right)
+        time.sleep(0.05)
 
+def on_press(key):
+    global running, cycle_thread
+    try:
+        if key.char and key.char.lower() == 'f':
+            if not running:
+                running = True
+                cycle_thread = threading.Thread(target=crystal_cycle)
+                cycle_thread.start()
+            else:
+                running = False
+                cycle_thread.join()
+    except AttributeError:
+        pass  # Special keys (not used here)
+
+if __name__ == "__main__":
+    print("Press F to start/stop crystal cycle loop.")
+    with Listener(on_press=on_press) as listener:
+        listener.join()
