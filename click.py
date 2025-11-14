b504136ef4883e684103d33b8bdb2e5aa0175a3e
TheCodingGuy0error0 <sebastian.minciunalux@gmail.com>
2025-11-14 13:38:09 +0100

    cycle

---
+++ click.py
@@ -0,0 +1,15 @@
+from pynput.keyboard import Controller as KeyboardController, Key
+from pynput.mouse import Button, Controller as MouseController
+import time
+
+mouse = MouseController()
+keyboard = KeyboardController()
+
+
+def crystal_cycle()
+    mouse.click(Button.left)
+    time.sleep(0.05)
+    mouse.click(Button.right)
+    time.sleep(0.05)
+
+
